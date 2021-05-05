c34438f27149712e3a9cebf72d932c508d5e4606
jerrenss <gerrenseow@gmail.com>
2021-05-05 23:50:17 +0800

    Update services section

--- src/constants/services.js
+++ src/constants/services.js
@@ -11,12 +11,12 @@ export default [
     id: 2,
     icon: <FaNode className="service-icon" />,
     title: 'Back-End Development',
-    text: `I'm most comfortable with Node JS development on the server side, with most experience in Express JS. I've also experimented with other frameworks such as Flask, Django and Laravel`,
+    text: `I'm most comfortable with Node JS development on the server side, with significant experience in Express JS. I've also experimented with other frameworks such as Flask, Django and Laravel.`,
   },
   {
     id: 3,
     icon: <FaDatabase className="service-icon" />,
     title: 'Database Management',
-    text: `I'm most comfortable with NoSQL database development using MongoDB and Google Firebase. Am in the midst of experimenting with SQL databases such as PostgreSQL and MySQL.`,
+    text: `I'm comfortable developing with both SQL and NoSQL databases. Worked namely with MongoDB, Firebase and PostgreSQL.`,
   },
 ]
